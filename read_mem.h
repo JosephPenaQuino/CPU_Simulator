0a3d85d0ec3ec1d1fa931a8aa81fd531d2aadd89
Joseph <joseph.pena@utec.edu.pe>
2019-06-25 17:29:42 -0500

    Implement void methods

--- read_mem.h
+++ read_mem.h
@@ -1,7 +1,3 @@
-//
-// Created by albin on 13/06/2019.
-//
-
 #ifndef CPU_SIMULATOR_READ_MEM_H
 #define CPU_SIMULATOR_READ_MEM_H
 
@@ -14,8 +10,14 @@ private:
     int size;
     vector<T> instructions_list;
 public:
-    read_mem(int size);
-    void get_instructions(int address);
+    read_mem(int size)
+    {
+
+    }
+
+    T get_instructions(int address) {
+        return T();
+    }
 };
 
 
