7ca831d03137f1a4ea75c00f1a731f4cff12de21
Joseph <joseph.pena@utec.edu.pe>
2019-06-29 21:42:47 -0500

    Add Parser

--- Parser.cpp
+++ Parser.cpp
@@ -1,5 +1,1 @@
-//
-// Created by albin on 27/06/2019.
-//
-
 #include "Parser.h"
--- Parser.h
+++ Parser.h
@@ -1,7 +1,3 @@
-//
-// Created by albin on 27/06/2019.
-//
-
 #ifndef CPU_SIMULATOR_PARSER_H
 #define CPU_SIMULATOR_PARSER_H
 
