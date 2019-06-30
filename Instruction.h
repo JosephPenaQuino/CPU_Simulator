9e0b42a5d583b01e1be3eb8d39e3fd685d1a729d
Joseph <joseph.pena@utec.edu.pe>
2019-06-29 23:42:17 -0500

    Move MIPS instructions to file MIPS_Instrctions

--- Instruction.h
+++ Instruction.h
@@ -3,7 +3,9 @@
 
 #include <cstdint>
 #include <cmath>
-#include "CPU.h"
+//#include "CPU.h"
+
+class CPU;
 
 struct CompR
 {
@@ -42,57 +44,12 @@ protected:
     complement comp;
 
 public:
+    Instruction() = default;
     explicit Instruction(uint32_t op_code);
     virtual void apply(CPU &my_cpu);
     virtual char type();
 };
 
-class Instruction_R: public Instruction
-{
-private:
 
-public:
-    Instruction_R(uint32_t op_code, uint32_t Rs, uint32_t Rt, uint32_t Rd, uint32_t SA, uint32_t Funct_code);
-    char type() override ;
-};
-
-class Instruction_I : public Instruction
-{
-private:
-
-public:
-    Instruction_I(uint32_t op_code, uint32_t Rs, uint32_t Rt, uint32_t comp_const);
-    char type() override;
-};
-
-class Instruction_J : public Instruction
-{
-private:
-
-public:
-    Instruction_J(uint32_t op_code, uint32_t jump_target);
-    char type() override ;
-};
-
-class Instruction_add : public Instruction_R
-{
-public:
-    Instruction_add(uint32_t Rs, uint32_t Rt, uint32_t Rd);
-    void apply(CPU &my_cpu) override;
-};
-
-class Instruction_sw : public Instruction_I
-{
-public:
-    Instruction_sw(uint32_t Rs, uint32_t Rt, uint32_t offset);
-    void apply(CPU &my_cpu) override;
-};
-
-class Instruction_lw : public Instruction_I
-{
-public:
-    Instruction_lw(uint32_t Rs, uint32_t Rt, uint32_t offset);
-    void apply(CPU &my_cpu) override;
-};
 
 #endif
